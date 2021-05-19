40c2be55c1cdcc5b1f7db95f6ab4c20186c2106a
Semir Zulic <semirzulic92@gmail.com>
2021-05-19 18:08:10 +0200

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore non nesciunt consequuntur eum laborum perferendis nam 
+      nihil excepturi, possimus, facilis reiciendis animi harum natus inventore dicta nostrum quae ea magni.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore non nesciunt consequuntur eum laborum perferendis nam 
+      nihil excepturi, possimus, facilis reiciendis animi harum natus inventore dicta nostrum quae ea magni.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore non nesciunt consequuntur eum laborum perferendis nam 
+      nihil excepturi, possimus, facilis reiciendis animi harum natus inventore dicta nostrum quae ea magni.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore non nesciunt consequuntur eum laborum perferendis nam 
+      nihil excepturi, possimus, facilis reiciendis animi harum natus inventore dicta nostrum quae ea magni.</p>
   </div>
 </template>
